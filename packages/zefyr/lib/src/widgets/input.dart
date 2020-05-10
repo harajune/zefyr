fbd3b2484d99592cd86b86e4126d3ba08428767d
harajune <harajune@gijutsuya.jp>
2020-05-10 11:59:29 +0900

    fix: implement new flutter sdk function. https://github.com/memspace/zefyr/issues/222

--- packages/zefyr/lib/src/widgets/input.dart
+++ packages/zefyr/lib/src/widgets/input.dart
@@ -15,7 +15,9 @@ class InputConnectionController implements TextInputClient {
   //
   // public members
   //
-
+  
+  @override
+  TextEditingValue get currentTextEditingValue => _lastKnownRemoteTextEditingValue;
   final RemoteValueChanged onValueChanged;
 
   /// Returns `true` if there is open input connection.
